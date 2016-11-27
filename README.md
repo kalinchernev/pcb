7451ec9dbd636daaffa3e2309ab3a7e0c37408c8
Kalin Chernev <kalin.chernev@gmail.com>
2016-11-27 13:11:27 +0000

    Basic client

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PCB
+
+- [List of bots](https://dev.botframework.com/bots)
+- [List of services](https://www.microsoft.com/cognitive-services)
---
+++ index.js
@@ -0,0 +1,22 @@
+var path = require('path')
+var os = require('os')
+
+var express = require('express')
+var app = express()
+
+var config = require(path.resolve(os.homedir() + '/config.json'))
+
+app.get('/', (req, res) => {
+  res.send(`
+      <!DOCTYPE html>
+        <html>
+          <body>
+            <iframe style="height:400px;" src='https://webchat.botframework.com/embed/tisha?s=${config.web_chat.key1}'></iframe>
+          </body>
+        </html>
+  `)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server fired at ${process.env.PORT}`)
+})
